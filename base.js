20f1289563a6a14b55185422833e22479015cbfa
wolegca <2738076308@qq.com>
2019-09-12 23:24:39 +0800

    add an alert function

--- base.js
+++ base.js
@@ -1,6 +1,57 @@
-window.onselectstart = function () {
-	return false;
+$(function () {
+	window.onselectstart = block;
+	window.ontouchstart = block;
+	window.ondragstart = block;
+	$('.msg-header').Drag($('.msg-container'));
+	$('.menu-container ul li span').on('click',function(){
+		message.alert('这里是','<div style="text-align:right"><div style="font-size:28px;height:70px;padding-top:40px;text-align:center">AHPU-老中医</div>的博客&nbsp;&nbsp;&nbsp;</div>');
+	});
+	function block() {
+		return false;
+	}
+});
+
+(function(){
+	$(window).on('resize',function(){
+		$('.msg-container').css({
+			"top":"50%",
+			"left":"50%"
+		});
+	});
+	$(document.body).on('click','.msg-footer span',function(){
+		var p = $(this).parent().parent();
+		p.css('animation','fadeOut .5s');
+		setTimeout(function(){
+			p.remove();
+		},500);
+	});
+})();
+
+var message = new function () {
+	this.alert = function (header, content) {
+		var id = uuid();
+		var html =
+			'<div class="msg-container" id="mc-' + id + '">' +
+			'<div class="msg-header" id="mh-'+id+'">' +
+			header +
+			'</div>' +
+			'<div class="msg-content">' +
+			content +
+			'</div>' +
+			'<div class="msg-footer">'+
+			'<span>关闭</span>'+
+			'</div>' +
+			'</div>'
+		$(document.body).append(html);
+		$('#mh-'+id).Drag($('#mc-'+id));
+		return id;
+	}
 }
-window.ontouchstart = function(){
-	return false;
+
+function uuid() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16 | 0,
+			v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
 }
---
+++ drag.js
@@ -0,0 +1,29 @@
+(function (document) {
+	//Usage: $("#id").Drag() 
+	//Author: hooyes
+	$.fn.Drag = function (target) {
+		var M = false;
+		var Rx, Ry;
+		var t = $(this);
+		t.mousedown(function (event) {
+				event.stopPropagation();
+				Rx = event.pageX - (parseInt(target.css("left")) || 0);
+				Ry = event.pageY - (parseInt(target.css("top")) || 0);
+				M = true;
+				scaleChanged = false;
+			})
+			.mouseup(function (event) {
+				M = false;
+				t.fadeTo(20, 1);
+			});
+		$(document).mousemove(function (event) {
+			if (M) {
+				target.css({
+					top: event.pageY - Ry,
+					left: event.pageX - Rx
+				});
+			}
+			scaleChanged = true;
+		});
+	}
+})(document);
